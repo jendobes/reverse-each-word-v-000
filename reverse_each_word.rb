8f05bc445b0558f07d0a09203ac95d568b9d5185
Jennifer Dobak <jdobak1@gmail.com>
2018-01-16 14:30:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,4 @@
-# def reverse_each_word (string)
-#  array = string.split
-#  array.collect {|word| word.reverse!}
-#  array.join(" ")
-#end
 
 def reverse_each_word (string)
-  string.split.collect {|word| word.reverse!}.join(" ")
+  string.split.each{|word| word.reverse!}.join(" ")
 end
